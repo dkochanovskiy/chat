ffa536848d67f851a7013f1f9a5b72ac550cb2b9
denis <dk@yakovlev.studio>
2021-02-16 17:12:50 +0300

    created migration create_message_table and RbacController

---
+++ migrations/m210216_135019_create_message_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%message}}`.
+ */
+class m210216_135019_create_message_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%message}}', [
+            'id' => $this->primaryKey(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%message}}');
+    }
+}
